aaf1d0b62a83304158f017d429c5a8ff25aaad3f
MercyMk <mercykyule25@gmail.com>
2023-09-07 09:43:18 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) 
+{
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of an int: %d byte(s)\n", sizeof(int));
+	printf("size of a short int: %d byte(s)\n", sizeof(short int));
+	printf("size of a long int: %d byte(s)\n", sizeof(long int));
+	return (0);
+}
